945827804a32e9efc7a3dfff673678ccf42ccf4c
fitsum <belay.fitsum@gmail.com>
2025-09-21 13:18:02 +0300

    creating base infra sg, vpc, igw 1.8

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -4,7 +4,7 @@
 data "aws_availability_zones" "aws_availability_zones" {}
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.1.0/16"
+  cidr_block = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
