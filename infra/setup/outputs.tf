d51bce526b3907fec6c0553418b51439228763aa
fitsum <belay.fitsum@gmail.com>
2025-09-22 11:51:26 +0300

    adding deploy to ec2 job1.1

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,7 +8,3 @@ output "ec2_public_ip" {
   value       = aws_instance.my_api_server.public_ip
   description = "The public IP address of the EC2 API server"
 }
-
-output "ec2_public_ip" {
-  value = aws_instance.my_api_server.public_ip
-}
