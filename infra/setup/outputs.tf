01ddda3b23e1d08b1ec2cb63fc0f9dc887d1ebc6
fitsum <belay.fitsum@gmail.com>
2025-09-23 22:46:00 +0300

    add missing output

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,3 +8,7 @@ output "ec2_public_ip" {
   value       = aws_instance.my_api_server.public_ip
   description = "The public IP address of the EC2 API server"
 }
+
+output "rds_db_name" {
+  value = aws_db_instance.db_instance.db_name
+}
