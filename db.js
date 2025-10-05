d39802b20fa3df764173a5d2317e2f6dfafe1a67
fitsum <belay.fitsum@gmail.com>
2025-10-05 11:48:30 +0300

    SSL auth for postgres

--- db.js
+++ db.js
@@ -7,8 +7,8 @@ const con = new Client({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }  
 })
 
 con.connect().then(()=> console.log("connected"))
